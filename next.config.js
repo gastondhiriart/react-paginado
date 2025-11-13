3b6c32bef58e9819ffced9dcbc23ebb1c6e8a4c2
Gaston d'Hiriart <gastondh91@gmail.com>
2025-11-13 19:06:33 -0300

    Add React compiler plugin configuration to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  plugins: ["react-compiler"],
+  rules: {
+    "react-compiler/react-compiler": "error",
+  },
   images: {
     domains: ["images.unsplash.com"],
   },
